b3036a4de3430fdad2fa8ce83db3a935f63fd949
Jamiu Ismail <32511206+phantware@users.noreply.github.com>
2022-10-07 23:46:12 +0100

    console.py file added

--- console.py
+++ console.py
@@ -37,6 +37,7 @@ class HBNBCommand(cmd.Cmd):
 
     def precmd(self, line):
         """Reformat command line for advanced command syntax.
+
         Usage: <class name>.<command>([<id> [<*args> or <**kwargs>]])
         (Brackets denote optional fields in usage example.)
         """
