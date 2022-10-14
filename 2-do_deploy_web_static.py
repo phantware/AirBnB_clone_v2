a62eba7aa79615e1f63cf362ce6c449a3ae89280
Jamiu Ismail <32511206+phantware@users.noreply.github.com>
2022-10-14 12:14:02 +0100

    user added to the file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import put
 from fabric.api import run
 
 env.hosts = ['18.207.241.255', '3.235.243.226']
-
+env.user = "ubuntu"
 
 def do_deploy(archive_path):
     """Distributes an archive to a web server.
